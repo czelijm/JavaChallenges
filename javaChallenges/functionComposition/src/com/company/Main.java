7999e123e1ce82eb00a221fabc758fc1b8ab54dc
czelijm <czelijm@outlook.com>
2020-11-09 22:50:44 +0100

    added the third task

--- javaChallenges/functionComposition/src/com/company/Main.java
+++ javaChallenges/functionComposition/src/com/company/Main.java
@@ -2,11 +2,12 @@ package com.company;
 
 import com.company.first.FirstDemo;
 import com.company.second.SecondDemo;
+import com.company.third.ThirdDemo;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         FirstDemo.main(args);
         SecondDemo.main(args);
-
+        ThirdDemo.main(args);
     }
 }
