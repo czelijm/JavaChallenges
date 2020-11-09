e006e1d6f5403fb293404ca7edd4abd6d56ac2ab
czelijm <czelijm@outlook.com>
2020-11-09 20:41:43 +0100

    add functionComposition

---
+++ javaChallenges/functionComposition/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import com.company.first.FirstDemo;
+import com.company.second.SecondDemo;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        FirstDemo.main(args);
+        SecondDemo.main(args);
+
+    }
+}
